3a3ebb7b209e93903d7effbe545cd2bec03a9607
sheemahamid <38783616+sheemahamid@users.noreply.github.com>
2018-05-03 14:06:08 +0000

    completed the code

--- rr.py
+++ rr.py
@@ -10,4 +10,25 @@ timeslice = 3
 rbtime=[0]*3
 i=0
 rbtime = list(btime) //copying the list
-print rbtime
+while(1):
+   done ="true"
+   for i in range (3):
+       if rbtime[i] > 0:
+           done = "false"
+	   if rbtime[i] > quantum:
+               t= t+quantum
+	       rbtime[i]=rbtime[i]-quantum
+	   else:
+   	       t=t+rbtime[i]
+	       waitingtime[i] = t - btime[i]-arrivaltime[i]
+               turnaround[i] = t - arrivaltime[i]
+	       rbtime[i]=0
+   if done== "true":
+     break
+for index in range(3):
+     print processes[index]
+     t = t + btime[index]
+     print("waiting time: "), waitingtime[index]
+     turnaround[index]=btime[index]+waitingtime[index]
+     print("turnaround :"), turnaround[index]
+     starttime=starttime+btime[index]
